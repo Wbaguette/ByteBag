f861bd1576473e7ba4961a0321f3d7d7ec697fb3
jean-pierre <jeanpierre@derbes.net>
2023-10-08 01:41:00 -0400

    what the fuck is happening how do i do this

---
+++ src/components/TitleBar.svelte
@@ -0,0 +1,23 @@
+<style>
+   .title-bar {
+      background-color: #333;
+      color: #fff;
+      padding: 0px;
+      margin: 0px;
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-start;
+      height: 10px;
+      width: 100%;
+   }
+</style>
+
+<div class="title-bar">
+   <button>
+      File
+   </button>
+
+   <button>
+      Edit
+   </button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
    import { invoke } from "@tauri-apps/api/tauri"
+   import TitleBar from "../components/TitleBar.svelte";
 
    let name: string
    let output: string
@@ -7,6 +8,8 @@
       output = await invoke('greet', { name: 'Jean-Pierre' })
    }
 </script>
+
+<TitleBar />
 
 <label for="name">Name</label>
 <input name="name" type="text" bind:value={name} on:change={greet}/>
