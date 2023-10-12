aacff3dfcf74637fa6cf658ff9cb57548531f0ae
jean-pierre <jeanpierre@derbes.net>
2023-10-12 00:16:08 -0400

    starting the editor part

---
+++ src/components/Editor.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+   import { onMount } from "svelte";
+   import {EditorState} from "@codemirror/state"
+   import {EditorView, keymap} from "@codemirror/view"
+   import {defaultKeymap} from "@codemirror/commands"
+
+   let cm;
+
+   
+</script>
+
+<style>
+
+</style>
+
