1836b02c3f98910cd79382415eea11dd7d086d3b
jean-pierre <jeanpierre@derbes.net>
2023-10-11 23:55:34 -0400

    title bar layout complete

--- src/components/TitleBar.svelte
+++ src/components/TitleBar.svelte
@@ -8,12 +8,24 @@
       background-color: rgb(13, 13, 13);
       user-select: none;
       display: flex;
-      justify-content: flex-start;
+      justify-content: space-between;
       position: fixed;
       align-items: center;
       top: 0;
       left: 0;
       right: 0;
+   }
+
+   .titlebar-left {
+      display: flex;
+      align-items: center;
+      margin: 0.4%;
+   }
+
+   .titlebar-right {
+      display: flex;
+      align-items: center;
+      margin: 0.4%;
    }
 
    .titlebar button {
@@ -38,30 +50,34 @@
 
 <div data-tauri-drag-region class="titlebar">
    <!-- Put an image of the logo here -->
+   <div class="titlebar-left">
+      <button>
+         File
+      </button>
    
-   <button>
-      File
-   </button>
+      <button>
+         Edit
+      </button>
+   
+      <button>
+         New
+      </button>
+   </div>
 
-   <button>
-      Edit
-   </button>
+   <div class="titlebar-right">
+      <button id="title-minimize" on:click={appWindow.minimize}>
+         <img src="https://api.iconify.design/mdi:window-minimize.svg?color=%23bab8b8" alt="minimize">
+      </button>
+   
+      <button id="title-maximize" on:click={async () => {
+         if (await appWindow.isMaximized()) { appWindow.unmaximize() } else { appWindow.maximize() }
+      }}>
+         <img src="https://api.iconify.design/mdi:window-maximize.svg?color=%23bab8b8" alt="maximize">
+      </button>
+   
+      <button id="title-close" on:click={appWindow.close}>
+         <img src="https://api.iconify.design/mdi:close.svg?color=%23bab8b8" alt="close">
+      </button>
+   </div>
 
-   <button>
-      New
-   </button>
-
-   <button id="title-minimize" on:click={appWindow.minimize}>
-      <img src="https://api.iconify.design/mdi:window-minimize.svg?color=%23bab8b8" alt="minimize">
-   </button>
-
-   <button id="title-maximize" on:click={async () => {
-      if (await appWindow.isMaximized()) { appWindow.unmaximize() } else { appWindow.maximize() }
-   }}>
-      <img src="https://api.iconify.design/mdi:window-maximize.svg?color=%23bab8b8" alt="maximize">
-   </button>
-
-   <button id="title-close" on:click={appWindow.close}>
-      <img src="https://api.iconify.design/mdi:close.svg?color=%23bab8b8" alt="close">
-   </button>
 </div>
